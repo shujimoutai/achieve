07f13d92b75d05da6ba38affa2ec7ffe3f56782d
SHUJI MOTAI <shujimoutai@gmail.com>
2017-04-01 00:19:43 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,26 @@
+class ContactsController < ApplicationController
+  
+  def new
+    if params[:back]
+      @contact = Contact.new(contacts_params)
+    else
+      @contact = Contact.new
+    end
+  end
+
+  def confirm
+    @contact=Contact.new(contacts_params)
+    render :new if @contact.invalid?
+  end
+
+  def create
+    Contact.create(contacts_params)
+    redirect_to root_path, notice: 'お問い合わせが完了しました！'
+  end
+  
+  private
+   def contacts_params
+     params.require(:contact).permit(:name,:email,:content)
+   end
+  
+end
