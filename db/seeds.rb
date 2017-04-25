a3c117f0d8f80e419c0ee593ccabf0461218fc69
SHUJI MOTAI <shujimoutai@gmail.com>
2017-04-25 12:50:02 +0000

    seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+100.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+n = 3
+while n <= 102
+  Blog.create(
+    title: "あああ",
+    content: "hoge",
+    user_id: n)
+  n = n + 1
+end
